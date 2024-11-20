56bed5f0c95ae1723d7f15624d2ef5196e73f8f4
isYangs <isYangs@foxmail.com>
2024-11-20 19:07:16 +0800

    feat: add patch for markdown-it-magic-link and update pnpm configuration

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,20 +11,11 @@ const { y: scroll } = useWindowScroll()
 
 <template>
   <header class="header z-40">
-    <RouterLink
-      class="w-12 h-12 absolute xl:fixed m-5 select-none outline-none"
-      to="/"
-      focusable="false"
-    >
+    <RouterLink class="w-12 h-12 absolute xl:fixed m-5 select-none outline-none" to="/" focusable="false">
       <Logo />
     </RouterLink>
-    <button
-      title="Scroll to top"
-      fixed right-3 bottom-3 w-10 h-10 hover:op100 rounded-full
-      hover-bg-hex-8883 transition duration-300 z-100 print:hidden
-      :class="scroll > 300 ? 'op30' : 'op0! pointer-events-none'"
-      @click="toTop()"
-    >
+    <button title="Scroll to top" fixed right-3 bottom-3 w-10 h-10 hover:op100 rounded-full hover-bg-hex-8883 transition
+      duration-300 z-100 print:hidden :class="scroll > 300 ? 'op30' : 'op0! pointer-events-none'" @click="toTop()">
       <div i-ri-arrow-up-line />
     </button>
     <nav class="nav">
@@ -49,7 +40,7 @@ const { y: scroll } = useWindowScroll()
           <div i-ph-github-logo-bold />
         </a>
         <a href="/feed.xml" target="_blank" title="订阅源" class="lt-md:hidden">
-          <div i-ph-rss-simple-bold style="font-size:1.25rem; margin: 0 -0.125rem;" />
+          <div i-ph-rss-simple-bold />
         </a>
         <ToggleTheme />
       </div>
@@ -76,7 +67,7 @@ const { y: scroll } = useWindowScroll()
   box-sizing: border-box;
 }
 
-.nav > * {
+.nav>* {
   margin: auto;
 }
 
@@ -104,7 +95,7 @@ const { y: scroll } = useWindowScroll()
   grid-auto-flow: column;
 }
 
-.nav .right > * {
+.nav .right>* {
   margin: auto;
 }
 </style>
